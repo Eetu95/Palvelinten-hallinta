e6d95447546b33810023a64b671300367ca4689e
Eetu95 <32093515+Eetu95@users.noreply.github.com>
2018-11-20 11:03:23 +0200

    Add a new script that makes salt-minions

--- salt-minion.sh
+++ salt-minion.sh
@@ -3,5 +3,5 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y install salt-minion
-sudo echo -e "master: \nid: minion4"|sudo tee /etc/salt/minion
+sudo echo -e "master: '#put your ip here'\nid: minion4"|sudo tee /etc/salt/minion
 sudo systemctl restart salt-minion.service
